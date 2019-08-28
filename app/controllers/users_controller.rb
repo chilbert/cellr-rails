9265a72b3fec7b7b8ea4325f8ea0678630cf25ca
chilbert <chase@chasehilbert.com>
2019-08-28 12:33:20 -0500

    Updating spec file and styling on login button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
       @user = User.new(user_params)
       if @user.save
         log_in @user
-        flash[:success] = "Welcome to Cellr, #{@user.name}."
+        flash[:success] = "Registration successful"
         redirect_to @user
       else
         render 'new'
