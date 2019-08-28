5df7c860ba1e00925ae0afd23ec350ade7f6b3ed
chilbert <chase@chasehilbert.com>
2019-08-28 00:16:51 -0500

    Updating Sessions and application controllers for login and signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  private
+     def logged_in?
+       !!current_user
+     end
+
+     def current_user
+       User.find_by(id: session[:user_id]) if session[:user_id]
+     end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(name: params[:user][:name])
+    user = User.find_by(email: params[:user][:email])
     user = user.try(:authenticate, params[:user][:password])
     return redirect_to(controller: 'sessions', action: 'new') unless user
     session[:user_id] = user.id
     @user = user
-    redirect_to controller: 'welcome', action: 'home'
+    redirect_to controller: 'static', action: 'home'
   end
 
   def destroy
