2fa5b16c1c4d18fe37d03c39969a7777ce2968a7
chilbert <chase@chasehilbert.com>
2019-08-27 23:30:04 -0500

    Updating static controller and home page

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#home'
+  root 'static#home'
   get '/signup' => 'users#new'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
