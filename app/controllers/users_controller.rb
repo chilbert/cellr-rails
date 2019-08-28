ebcc9ccae60160a8138facd5131c3ef8fd440a6f
chilbert <chase@chasehilbert.com>
2019-08-28 10:49:04 -0500

    Updating logged in nav, user views and spec file for requirements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,39 @@
 class UsersController < ApplicationController
 
+  before_action :logged_in_user, only: [:edit, :update, :show]
+  before_action :correct_user,   only: [:edit, :update, :show]
+
 
   def show
-    @user = User.find(params[:id])
-  end
+      @user = User.find(params[:id])
+    end
 
-  def new
-    @user = User.new
-  end
+    def new
+      @user = User.new
+    end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      log_in @user
-      flash[:success] = "Welcome to Cellr."
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        log_in @user
+        flash[:success] = "Welcome to Cellr, #{@user.name}."
+        redirect_to @user
+      else
+        render 'new'
+      end
+    end
+
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
       redirect_to @user
     else
-      render 'new'
+      render 'edit'
     end
-  end
-
-  def edit
-    @user = User.find(params[:id])
   end
 
 
@@ -30,4 +42,17 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation, :name)
   end
+
+  def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+     redirect_to(root_url) unless current_user?(@user)
+    end
+
 end
