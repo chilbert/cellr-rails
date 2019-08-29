2925becd1ecd7f8af592c713397b9e970892c72c
chilbert <chase@chasehilbert.com>
2019-08-29 16:30:02 -0500

    Updating bottles model with validations and removing number :id from bottle form submission

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -3,4 +3,8 @@ class Bottle < ApplicationRecord
   has_many :users, through: :comments
   belongs_to :winery
 
+  validates :vintage, length: { is: 4 }
+  validates :price, numericality: { only_integer: true }
+
+
 end
